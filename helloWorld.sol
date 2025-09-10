bb79126840e8141256eb504786019b7f84783aff
smaclarn <msravanch@gmail.com>
2025-09-10 14:37:03 +0530

    helloWorld.sol

---
+++ helloWorld.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+// compiler version must be greater than or equal to 0.8.26 and less than 0.9.0
+pragma solidity ^0.8.26;
+
+contract HelloWorld {
+    function sayH() public pure returns(string memory)
+{
+    return "HI";
+}}
